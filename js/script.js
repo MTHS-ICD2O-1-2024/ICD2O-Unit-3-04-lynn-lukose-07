519ac3ae198b7c79763178e243231a6f46849ae3
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-09 09:12:44 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
+// Updated by Lynn Lukose
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function convertFahrenheitToCelsius() {
+  // input
+  const degreesInFahrenheit = parseFloat(
+    document.getElementById("temp-f").value
+  )
+
+  // process
+  const degreesInCelsius = (degreesInFahrenheit - 32) * (5 / 9)
+
+  // output
+  document.getElementById("temperature").innerHTML =
+    "The temperature in Celsius is: " + degreesInCelsius.toFixed(3) + " °C"
+}
